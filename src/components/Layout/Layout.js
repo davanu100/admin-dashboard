2bada470c21599cc292b744aeb3b8433a54a3c94
ANURAG THAKUR <davanu100@gmail.com>
2020-06-30 17:52:15 +0530

    Initial dashboard template added

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxilliary';
+import Dashboard from '../Dashboard-components/Dashboard';
+
+
+const layout = (props) => (
+    <Aux>
+        <Dashboard />
+    </Aux>
+);
+
+export default layout;
